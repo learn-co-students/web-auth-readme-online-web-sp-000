461b82d3bbe4ca2e80cc8ae47370eb3ceb40e156
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-07-05 23:38:39 +0000

    Done.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,14 @@
 class SearchesController < ApplicationController
   def search
+    @resp = Faraday.get("https://api.foursquare.com/v2/users/self/friends") do |req|
+      req.params["oauth_token"] = session["token"]
+      req.params['v'] = '20160201'
+    end
+
+    body = JSON.parse(@reps.body)
+    @friends = body["friends"]["items"]
+
+    render :friends
   end
 
   def foursquare
